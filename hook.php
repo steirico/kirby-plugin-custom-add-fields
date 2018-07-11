ffb10b99ae5f888922752947b395b7ab6048be6c
steirico <steirico@users.noreply.github.com>
2018-07-11 22:56:20 +0200

    Add panel.page.create hook

---
+++ hook.php
@@ -0,0 +1,12 @@
+<?php
+if(!function_exists('kirby')) return;
+kirby()->hook('panel.page.create', function($page) {
+
+
+    $modelName = array_values(Page::$models)[0];
+    $modelName = preg_replace('/\\\\(.+)/m', '$1', $modelName);
+
+    if(method_exists($modelName, 'hookPageCreate')){
+        $modelName::hookPageCreate($page);
+    }    
+});
