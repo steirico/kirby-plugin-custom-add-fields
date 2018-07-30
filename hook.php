336422f9a3ca79a1b28a9baff94f7b09ca629d8f
steirico <steirico@users.noreply.github.com>
2018-07-30 07:46:21 +0200

    Create page instance of correct class and call hookPageCreate method

--- hook.php
+++ hook.php
@@ -2,11 +2,11 @@
 if(!function_exists('kirby')) return;
 kirby()->hook('panel.page.create', function($page) {
 
-
     $modelName = array_values(Page::$models)[0];
     $modelName = preg_replace('/\\\\(.+)/m', '$1', $modelName);
 
     if(method_exists($modelName, 'hookPageCreate')){
-        $modelName::hookPageCreate($page);
+        $pageInstance = new $modelName($page->parent(), $page->dirname());
+        $pageInstance->hookPageCreate();
     }    
 });
