1b18cd05c59c3f1c352c6b3f8482b8cdd94e9613
steirico <steirico@users.noreply.github.com>
2019-04-25 07:00:01 +0200

    Genereta fields correctly as in a field section

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
 use Kirby\Cms\Blueprint;
+use Kirby\Cms\Section;
 
 Kirby::plugin('steirico/kirby-plugin-custom-add-fields', [
     'api' => [
@@ -18,14 +19,28 @@ Kirby::plugin('steirico/kirby-plugin-custom-add-fields', [
                     foreach ($templates as $template) {
                         try {
                             $props = Blueprint::load('pages/' . $template['name']);
+                            $addFields = A::get($props, 'addFields', null);
+                            if($addFields){
+                                $title = A::get($addFields, 'title', null);
+                                $attr = [
+                                    'model' => $object,
+                                    'fields' => $addFields
+                                ];
+                                $addSection = new Section('fields', $attr);
+                                $addFields = $addSection->fields();
+
+                                if($title){
+                                    $addFields['title'] = $title;
+                                }
+
+                            }
                             array_push($result, [
                                 'name'  => $template['name'],
                                 'title' => $template['title'],
-                                'addFields' => $props['addFields'],
+                                'addFields' => $addFields,
                                 'parentPage' => $id
                             ]);
                         } catch (Throwable $e) {
-                            $result = $templates;
                         }
                     }
                     return $result;
