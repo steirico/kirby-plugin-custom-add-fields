acb10ba438b9dfbc921a822076575a4defb0e0e0
steirico <steirico@users.noreply.github.com>
2021-04-12 23:13:00 +0200

    Fix component overwrite

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import CustomAddDialog from './CustomAddDialog.vue'
 
 panel.plugin("steirico/kirby-plugin-custom-add-fields", {
   components: {
-    'k-page-create-dialog': CustomAddDialog,
-  }
+    'k-page-create-dialog': CustomAddDialog
+  },
+  use: [
+    function(Vue) {
+      const
+        VUE_COMPONENTS = Vue.options.components;
+
+      Object.keys(VUE_COMPONENTS).forEach(componentName => {
+        const COMPONENT = {
+          components: {
+            'k-page-create-dialog': CustomAddDialog
+          },
+          extends: VUE_COMPONENTS[componentName]
+        };
+        Vue.component(componentName, COMPONENT);
+      });
+    }
+  ]
 });
