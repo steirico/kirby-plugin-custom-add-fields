0af07dd2386468ca71d1a536c7f857294a382ca2
steirico <steirico@users.noreply.github.com>
2021-10-30 09:12:56 +0200

    Merge legacy code from c05171767f33b90ef20b8b18f0f59bbe63b2b605

--- config.php
+++ config.php
@@ -32,6 +32,124 @@ Kirby::plugin('steirico/kirby-plugin-custom-add-fields', [
     'api' => [
         'routes' => [
             [
+                'pattern' => [
+                    'site/children/blueprints/add-fields',
+                    'site/blueprints/add-fields',
+                    'pages/(:any)/children/blueprints/add-fields',
+                    'pages/(:any)/blueprints/add-fields',
+                ],
+                'method' => 'GET',
+                'filter' => 'auth',
+                'action'  => function (string $id = '') {
+                    $result = [];
+                    $object = $id == '' ? $this->site() : $this->page($id);
+                    $templates = $object->blueprints($this->requestQuery('section'));
+
+                    $parentProps = Blueprint::load($object->blueprint()->name());
+                    $parentAddFields = A::get($parentProps, 'addFields', null);
+
+                    $dialogProperties = A::get($parentAddFields, '__dialog', null);
+                    $skipDialog  = A::get($dialogProperties, 'skip', false);
+
+                    $forcedTemplateFieldName = option('steirico.kirby-plugin-custom-add-fields.forcedTemplate.fieldName');
+                    $forcedTemplateFieldName = $forcedTemplateFieldName ? $forcedTemplateFieldName : '';
+                    $hasForcedTemplate = $object->content()->has($forcedTemplateFieldName);
+                    $forcedTemplate = '';
+
+                    if($hasForcedTemplate){
+                        $forcedTemplate = $object->{$forcedTemplateFieldName}()->value();
+                    }
+
+                    $forcedTemplate = $forcedTemplate != '' ? $forcedTemplate : A::get($dialogProperties, 'forcedTemplate', '');
+
+                    if ($skipDialog) {
+                        if ($forcedTemplate == ''){
+                            throw new Exception("Set 'forcedTemplate' in order to skip add dialog.");
+                        } else {
+                            $now = time();
+                            $result = array(
+                                'skipDialog' => true,
+                                'page' => array(
+                                    'template'  => $forcedTemplate,
+                                    'slug' => $now,
+                                    'content' => array (
+                                        'title' => $now,
+                                    )
+                                )
+                            );
+                            return $result;
+                        }
+                    }
+
+                    $forceTemplateSelection = option('steirico.kirby-plugin-custom-add-fields.forceTemplateSelectionField');
+                    if(!is_bool($forceTemplateSelection)) {
+                        $version = preg_replace('/.*(\d+\.\d+\.\d+).*/m', '$1', $this->kirby()->version());
+                        $forceTemplateSelection = version_compare($version, '3.5.0', '<');
+                    }
+                    $result = array(
+                        'forceTemplateSelection' => $forceTemplateSelection,
+                        'templates' => []
+                    );
+
+                    foreach ($templates as $template) {
+                        if($hasForcedTemplate && $template['name'] != $forcedTemplate){
+                            continue;
+                        }
+                        try {
+                            $props = Blueprint::load('pages/' . $template['name']);
+                            $addFields = A::get($props, 'addFields', null);
+                            if($addFields){
+                                $dialogProperties = A::get($addFields, '__dialog', null);
+                                if($dialogProperties) {
+                                    $redirectToNewPage = A::get($addFields['__dialog'], 'redirect', false);
+                                    unset($addFields['__dialog']);
+                                } else {
+                                    $redirectToNewPage = false;
+                                }
+
+
+                                if(!empty($addFields)) {
+                                    $fieldProps = Blueprint::fieldsProps($addFields);
+                                    $fieldOrder = array_change_key_case($fieldProps, CASE_LOWER);
+                                    $title = A::get($fieldProps, 'title', null);
+                                    if($title) {
+                                        $fieldProps["kirby-plugin-custom-add-fields-title"] = $title;
+                                    }
+                                    $attr = [
+                                        'model' => $object,
+                                        'fields' => $fieldProps
+                                    ];
+                                    $addSection = new Section('fields', $attr);
+                                    $addFields = $addSection->fields();
+
+                                    if($title){
+                                        $addFields['title'] = $addFields["kirby-plugin-custom-add-fields-title"];
+                                        unset($addFields["kirby-plugin-custom-add-fields-title"]);
+                                        $addFields['title']['name'] = 'title';
+                                    }
+
+                                    $addFields = array_replace($fieldOrder, $addFields);
+                                }
+
+                            } else {
+                                $redirectToNewPage = true;
+                            }
+                            array_push($result['templates'], [
+                                'name'  => $template['name'],
+                                'title' => $template['title'],
+                                'addFields' => $addFields,
+                                'options'=> [
+                                    'redirectToNewPage'=> $redirectToNewPage
+                                ],
+                                'parentPage' => $id
+                            ]);
+                        } catch (Throwable $e) {
+                        }
+                    }
+                    return $result;
+                }
+            ],
+            [
                 'pattern' => 'pages/(:any)/addsections/(:any)',
                 'method'  => 'GET',
                 'action'  => function (string $id, string $sectionName) {
--- index.js
+++ index.js
@@ -101,6 +101,235 @@ const PAGE_CREATE_DIALOG = {
   }
 };
 
+const LEGACY_PAGE_CREATE_DIALOG = {
+  extends: 'k-page-create-dialog',
+  template: `
+    <k-dialog
+      ref="dialog"
+      :submit-button="$t('page.draft.create')"
+      :notification="notification"
+      size="medium"
+      theme="positive"
+      @submit="$refs.form.submit()"
+    >
+      <k-form
+        ref="form"
+        :fields="fields"
+        :novalidate="true"
+        :key="template"
+        v-model="page"
+        @submit="submit"
+        @input="input"
+      />
+    </k-dialog>
+  `,
+  data() {
+    return {
+      notification: null,
+      parent: null,
+      section: null,
+      templates: [],
+      template: '',
+      page: {},
+      addFields: {}
+    };
+  },
+
+  computed: {
+    fields() {
+      var
+        fields = {},
+        field = {},
+        endpoint = this.$route.path,
+        section = 'addFields';
+
+      if(this.addFields) {
+        fields = this.addFields;
+      } else {
+        fields = {
+          title: {
+            label: this.$t("title"),
+            type: "text",
+            required: true,
+            icon: "title"
+          },
+          slug: {
+            label: this.$t("slug"),
+            type: "text",
+            required: true,
+            counter: false,
+            icon: "url"
+          }
+        }
+      }
+
+      if (this.templates.length > 1 || this.forceTemplateSelection || config.debug) {
+        fields.template = {
+          name: "template",
+          label: this.$t("template"),
+          type: "select",
+          disabled: this.templates.length === 1,
+          required: true,
+          icon: "code",
+          empty: false,
+          options: this.templates
+        }
+      }
+
+      Object.keys(fields).forEach(name => {
+        field = fields[name];
+
+        if (name != "title" && name != "template" && this.page[name] === undefined){
+          if (field.default !== null && field.default !== undefined) {
+            this.$set(this.page, name, this.$helper.clone(field.default));
+          } else {
+            this.$set(this.page, name, null);
+          }
+        }
+
+        if (name === "title" && this.page[name] === "") {
+          if (field.default !== null && field.default !== undefined) {
+            this.$set(this.page, name, this.$helper.clone(field.default));
+          } else {
+            this.$set(this.page, name, "");
+          }
+        }
+
+        field.section = section;
+        field.endpoints = {
+          field: endpoint + "/addfields/" + this.template + "/" + name,
+          section: endpoint + "/addsections/" + this.template + "/" + section,
+          model: endpoint
+        };
+      });
+
+      return fields;
+    }
+  },
+
+  methods: {
+    open(parent, blueprintApi, section) {
+      this.parent  = parent;
+      this.section = section;
+
+      this.$api
+        .get(blueprintApi + '/add-fields', {section: section})
+        .then(response => {
+          if(response.skipDialog){
+            this.submit(response);
+            return;
+          }
+          this.forceTemplateSelection = response.forceTemplateSelection || false;
+          this.templates = response.templates.map(blueprint => {
+            return {
+              value: blueprint.name,
+              text: blueprint.title,
+              addFields: blueprint.addFields,
+              options: blueprint.options
+            };
+          });
+
+          if (this.templates[0]) {
+            this.page.template = this.templates[0].value;
+            this.template = this.templates[0].value;
+            this.addFields = this.templates[0].addFields;
+            this.options = this.templates[0].options;
+          }
+
+          this.$refs.dialog.open();
+        })
+        .catch(error => {
+          this.$store.dispatch("notification/error", error);
+        });
+    },
+
+    input() {
+      if(this.page.template !== this.template){
+        var
+          oTemplate = {},
+          template = this.page.template;
+
+        this.template  = template;
+
+        oTemplate = this.templates.find(function(tpl){
+          return tpl.value === template;
+        });
+        this.addFields = oTemplate.addFields;
+        this.options = oTemplate.options;
+        this.$set(this.page, "template", template);
+      }
+    },
+
+    isValid() {
+      var
+        form = this.$refs.form,
+        errors = {},
+        invalid = false;
+
+      if(form) {
+        form.novalidate = false;
+        errors = form.$refs.fields.errors;
+        invalid = true;
+
+        Object.keys(errors).some(field => {
+          var error = errors[field];
+          invalid = error.$pending || error.$invalid || error.$error;
+          return invalid;
+        });
+        return !invalid;
+      } else {
+        return !invalid;
+      }
+    },
+
+    submit(pageData) {
+      if (this.isValid()){
+        var data = {};
+        var route = '';
+
+        if(pageData.skipDialog){
+          data = pageData.page;
+        } else {
+          data = {
+            template: this.page.template,
+            slug: this.page.slug || Date.now(),
+            content: Object.assign({}, this.page)
+          };
+        }
+
+        delete data.content.addFields;
+        delete data.content.template;
+        delete data.content.slug;
+
+        this.$api
+          .post(this.parent + "/children", data)
+          .then(page => {
+            if(this.options && this.options.redirectToNewPage) {
+              if(this.options.redirectToNewPage === true) {
+                route = this.$api.pages.link(page.id);
+              } else if (this.options.redirectToNewPage !== false) {
+                route = this.$api.pages.link(this.options.redirectToNewPage);
+              }
+            } else {
+              route = page.parent ? this.$api.pages.link(page.parent.id) : '/';
+            }
+
+            this.success({
+              route: route,
+              message: ":)",
+              event: "page.create"
+            });
+          })
+          .catch(error => {
+            this.$refs.dialog.error(error.message);
+          });
+      } else {
+        this.$refs.dialog.error("Form is not valid");
+      }
+    }
+  }
+};
+
 panel.plugin("steirico/kirby-plugin-custom-add-fields", {
   components: {
     'k-page-create-dialog': PAGE_CREATE_DIALOG
